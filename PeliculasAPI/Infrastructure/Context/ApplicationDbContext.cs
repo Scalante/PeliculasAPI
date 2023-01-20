e4689d9ce6286375bc49c006d004e2c185e8c4cf
Scalante <rubendarioscalante@gmail.com>
2023-01-20 14:41:52 -0500

    Se crea el proyecto PeliculasAPI, se crea 1 migración, se crea la estructura de carpetas Core, Infrastructure y demás subcarpetas.

---
+++ PeliculasAPI/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.Core.Entities;
+
+namespace PeliculasAPI.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Genero> Generos { get; set; }
+    }
+}
---
+++ PeliculasAPI/Startup.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using PeliculasAPI.Infrastructure.Context;
+using System.Reflection;
+
+namespace PeliculasAPI
+{
+    public class Startup
+    {
+        public static IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Registrar DbContext
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+            //Permite evitar los ciclos infinitos en las relaciones de clases
+            //services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+            //No muestra en la respuesta de la API los atributos que tienen como valor NULL, esto funciona con Controllers
+            services.AddControllers().AddJsonOptions(opciones => opciones.JsonSerializerOptions.DefaultIgnoreCondition
+                            = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull);
+
+
+            //Configuración de Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                {
+                    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                    c.IncludeXmlComments(xmlPath);
+                }
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Blog API", Version = "v1" });
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsRule", rule => {
+                    rule.AllowAnyHeader().AllowAnyMethod().WithOrigins("*");
+                });
+            });
+        }
+
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseCors("CorsRule");
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
